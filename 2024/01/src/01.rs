f42d0ff85ffddc94df4762edd178724d432fd56c
Peter Volgyesi <peter.volgyesi@gmail.com>
2024-11-30 23:26:04 -0600

    2024/01

---
+++ 2024/01/src/01.rs
@@ -0,0 +1,30 @@
+use itertools::{multiunzip, Itertools};
+aoc::parts!(1, 2);
+
+fn parse(input: aoc::Input) -> (Vec<i32>, Vec<i32>) {
+    multiunzip(input.lines().map(|l| {
+        l.split_whitespace()
+            .map(|s| s.parse::<i32>().unwrap())
+            .collect_tuple()
+            .unwrap()
+    }))
+}
+
+fn part_1(input: aoc::Input) -> impl ToString {
+    let (mut left, mut right) = parse(input);
+    left.sort();
+    right.sort();
+
+    left.iter()
+        .zip(right)
+        .map(|(l, r)| (l - r).abs())
+        .sum::<i32>()
+}
+
+fn part_2(input: aoc::Input) -> impl ToString {
+    let (left, right) = parse(input);
+
+    left.iter()
+        .map(|l| l * right.iter().filter(|&r| r == l).count() as i32)
+        .sum::<i32>()
+}
